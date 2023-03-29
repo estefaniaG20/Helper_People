9d010305b0a678dcf84bc0560a27a014193d364e
Angie Mejia <mejiagonzaleza881@gmail.com>
2023-03-29 18:37:19 -0500

    Organizado Header del Componente Rol

--- src/Components/Layouts/Rol/HeaderR.jsx
+++ src/Components/Layouts/Rol/HeaderR.jsx
@@ -4,12 +4,8 @@ export const HeaderR = () => {
   return (
     <>
     <div className="headerR">
-      <div className="tituloa">
+      <div className="titulo">
         <h1>Helper People</h1>
-      </div>
-      <div className="divHeaderR">
-            <a href="about-us" className="bottom">QUIENES SOMOS</a>
-            <a href="know-us" className="bottom">CONOCENOS</a>
       </div>
     </div>
     </>
